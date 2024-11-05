62996f104a42a62f9f42f004cb002522f8f9655d
Pankaj Sahu <81400644+pankajsao11@users.noreply.github.com>
2024-11-05 12:53:51 +0530

    Created api-gw.tf

---
+++ api-gw.tf
@@ -0,0 +1,37 @@
+# API Gateway
+resource "aws_api_gateway_rest_api" "api_gw" {
+  name = "serverless-webApi"
+}
+
+resource "aws_api_gateway_resource" "api_resource" {
+  path_part   = "resource"
+  parent_id   = aws_api_gateway_rest_api.api_gw.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api_gw.id
+}
+
+resource "aws_api_gateway_method" "api_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gw.id
+  resource_id   = aws_api_gateway_resource.api_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gw.id
+  resource_id             = aws_api_gateway_resource.api_resource.id
+  http_method             = aws_api_gateway_method.api_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api_gw.id}/*/${aws_api_gateway_method.api_method.http_method}${aws_api_gateway_resource.api_resource.path}"
+}
