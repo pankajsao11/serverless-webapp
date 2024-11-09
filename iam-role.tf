f40bb11cc5571d480a887215c115585214b889e3
pankajsao11 <pankajsao11@gmail.com>
2024-11-09 16:32:08 +0530

    feat: terraform files creation completed

---
+++ iam-role.tf
@@ -0,0 +1,41 @@
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["dynamodb:*", "cloudwatch:*", "lambda:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "webapp_role" {
+  name = "serverless-webapp-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Role_usage = "Permission for DynamoDB and Cloudwatch"
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name   = "permissions_attachment"
+  policy = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "permission-attach" {
+  role       = aws_iam_role.webapp_role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
